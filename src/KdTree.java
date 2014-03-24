724fed3daa4e490f6d35a09b32d74edca088f909
Maxim Ovchinnikov <maxovnn@gmail.com>
2014-03-24 09:19:47 +0400

    First commit

---
+++ src/KdTree.java
@@ -0,0 +1,65 @@
+public class KdTree {
+    /*
+     * construct an empty set of points
+     */
+    public KdTree() {
+        
+    }
+
+    /**
+     * @return is the set empty?
+     */
+    public boolean isEmpty() {
+    }
+
+    /**
+     * @return number of points in the set
+     */
+    public int size() {
+        
+    }
+
+    /**
+     * add the point p to the set (if it is not already in the set)
+     * @param p
+     */
+    public void insert(Point2D p) {
+        
+    }
+
+    /**
+     * @param p
+     * @return does the set contain the point p?
+     */
+    public boolean contains(Point2D p) {
+        
+    }
+
+    /**
+     * draw all of the points to standard draw
+     */
+    public void draw() {
+        
+    }
+
+    /**
+     * @param rect
+     * @return all points in the set that are inside the rectangle
+     */
+    public Iterable<Point2D> range(RectHV rect) {
+        
+    }
+
+    /**
+     * 
+     * @param p
+     * @return a nearest neighbor in the set to p; null if set is empty
+     */
+    public Point2D nearest(Point2D p) {
+        
+    }
+    
+    public static void main(String[] args){
+    }
+
+}
